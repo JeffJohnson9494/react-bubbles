ebf3e2284d6d06793c10462763ae6bb1b5c82304
JeffJohnson <jeff.jj.9494@gmail.com>
2019-10-14 10:26:07 -0600

    oops app was broken on last push i was fiddeling around with code trying to figure out the login and caused an error. its fixed now

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import {Router} from 'react-router-dom';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
